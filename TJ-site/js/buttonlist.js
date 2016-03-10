4092cb9602d8a65c1aa9b14e62da8e8a59cce9fc
Conrad Lim <conradlim@waddup-2.local>
2016-03-09 22:45:30 -0800

    uih

--- TJ-site/js/buttonlist.js
+++ TJ-site/js/buttonlist.js
@@ -5,16 +5,4 @@ $("#shirtorder").change(function(){
 
 })
 
-$("#shirtorderTwo").change(function(){
-   correspondingID = $(this).find(":selected").val()
-   $(".style-sub-2").hide();
-   $("#" + correspondingID).show();
 
-})
-
-$("#shirtorderThree").change(function(){
-   correspondingID = $(this).find(":selected").val()
-   $(".style-sub-3").hide();
-   $("#" + correspondingID).show();
-
-})
