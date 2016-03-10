6b7e32f47f4954f19f30f3431b4e3cb2b618cbb9
Conrad Lim <conradlim@waddup-2.local>
2016-03-09 22:22:46 -0800

    style name updated

--- TJ-site/js/buttonlist.js
+++ TJ-site/js/buttonlist.js
@@ -1,6 +1,48 @@
-$("#Rank").change(function(){
+$("#shirtorder").change(function(){
    correspondingID = $(this).find(":selected").val()
    $(".style-sub-1").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-2").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-3").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-4").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-5").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-6").hide();
+   $("#" + correspondingID).show();
+
+})
+
+$("#shirtorder").change(function(){
+   correspondingID = $(this).find(":selected").val()
+   $(".style-sub-7").hide();
    $("#" + correspondingID).show();
 
 })
