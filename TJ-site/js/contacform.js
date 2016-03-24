9e23be4822f6f72b859951b5cffe44a6b3354eba
Conrad Lim <conradp04@yahoo.com>
2016-03-24 12:43:06 -0700

    Update contacform.js

--- TJ-site/js/contacform.js
+++ TJ-site/js/contacform.js
@@ -4,13 +4,14 @@
         
         
 //function asd(a)
-{
-    if(a==1)
-    document.getElementById("asd").style.display="none";
-    else
-    document.getElementById("asd").style.display="block";
-}
-
+///{
+  
+///    if(a==1)
+   /// document.getElementById("asd").style.display="none";
+///    else
+///    document.getElementById("asd").style.display="block";
+///}
+///
    $("#previousbutton").click(function() {
     $("#form_sub_container1").show();
     $("#form_sub_container2").hide(); })
